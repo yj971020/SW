ff7a452164f06ecae2c0430eb550f264d76ae856
note09 <note09@DESKTOP-D8U0RH9>
2022-06-09 17:13:20 +0900

    Java review commit

--- JavaStudy/src/ch04/ch2/Dog.java
+++ JavaStudy/src/ch04/ch2/Dog.java
@@ -3,8 +3,7 @@ package ch04.ch2;
 //다형성
 public class Dog extends Animal{
 	@Override
-	void speak() { //signature - 메소드명 : 타입과 매개변수 타입. 수, 순서
-		
+	void speak() { 
 		System.out.println("강아지 멍멍");;
 	}
 	
